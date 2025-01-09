3847d016e92e2ee1e284acb07008b07fcca39bc9
xFerchoVJ <fer_elsword@hotmail.com>
2025-01-09 00:45:37 -0600

    Finished challenge

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,35 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { Invoice } from './invoice';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [FormsModule, HttpClientModule, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'frontend_contalink';
+  startDate: string = '';
+  endDate: string = '';
+  sales: Invoice[] | null = null;
+
+  constructor(private http: HttpClient) {}
+
+  onSubmit() {
+    if (this.startDate && this.endDate) {
+      this.sales = null;
+      const url = `http://localhost:3000/invoices?start_date=${this.startDate}&end_date=${this.endDate}`;
+      this.http.get(url).subscribe({
+        next: (data: any) => {
+          this.sales = data;
+        },
+        error: (err) => {
+          console.error('Error fetching data:', err);
+        },
+      });
+    }
+  }
 }
---
+++ src/app/invoice.ts
@@ -0,0 +1,7 @@
+export interface Invoice {
+  id: number;
+  invoice_number: string;
+  total: number;
+  invoice_date: string;
+  status: string;
+}
